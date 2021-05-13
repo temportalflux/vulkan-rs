26ed53ff45989d16f632977be3ace5a28fbb337a
Dustin Yost <dustin.yost.t@gmail.com>
2021-05-13 08:46:54 -0400

    Move boid crates to demo folder

--- boid/src/ecs/systems.rs
+++
@@ -1,6 +0,0 @@
-mod instance_collector;
-pub use instance_collector::*;
-mod move_entities;
-pub use move_entities::*;
-mod world_bounds;
-pub use world_bounds::*;
---
+++ demo/boid/src/ecs/systems.rs
@@ -0,0 +1,6 @@
+mod instance_collector;
+pub use instance_collector::*;
+mod move_entities;
+pub use move_entities::*;
+mod world_bounds;
+pub use world_bounds::*;
