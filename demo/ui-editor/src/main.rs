07f1722ac87733406992d3153a3a10d88574f7e9
Dustin Yost <dustin.yost.t@gmail.com>
2021-05-13 08:35:53 -0400

    Move triangle demo into demo folder

--- demo/ui-editor/src/main.rs
+++ demo/ui-editor/src/main.rs
@@ -2,7 +2,7 @@ use temportal_engine as engine;
 use temportal_engine_editor as editor;
 
 fn main() -> engine::utility::VoidResult {
-	let editor = editor::Editor::new::<ui::UIDemo>()?;
+	let editor = editor::Editor::new::<demo_ui::UIDemo>()?;
 	let _ = editor.borrow().run_commandlets()?;
 	Ok(())
 }
