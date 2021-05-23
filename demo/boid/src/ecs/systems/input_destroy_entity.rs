33822f540bc590cbdd35f9a1f6f76140dd487cbd
Dustin Yost <dustin.yost.t@gmail.com>
2021-05-23 13:28:24 -0400

    Add entity destruction

--- demo/boid/src/ecs/systems/input_destroy_entity.rs
+++ demo/boid/src/ecs/systems/input_destroy_entity.rs
@@ -1,6 +1,6 @@
 use crate::{
 	ecs::{self, NamedSystem},
-	engine::input,
+	engine::{input, rand::{self, Rng}},
 };
 
 pub struct InputDestroyEntity {}
@@ -18,12 +18,42 @@ impl NamedSystem for InputDestroyEntity {
 }
 
 impl<'a> ecs::System<'a> for InputDestroyEntity {
-	type SystemData = ();
-	fn run(&mut self, _: Self::SystemData) {
+	type SystemData = ecs::Entities<'a>;
+	fn run(&mut self, entities: Self::SystemData) {
+		use ecs::Join;
+		static DESTROY_COUNT: usize = 10;
 		if input::System::read()
 			.is_key_pressed(input::KeyCode::Minus, std::time::Duration::from_millis(1))
 		{
-			log::debug!("destroy entity");
+			let mut rng = rand::thread_rng();
+			let ent_count = entities.join().count();
+
+			let mut ent_indices = Vec::new();
+			if ent_count <= DESTROY_COUNT {
+				ent_indices = (0..ent_count).collect();
+			}
+			else
+			{
+				while ent_indices.len() < DESTROY_COUNT {
+					let next_index = rng.gen_range(0..ent_count);
+					if !ent_indices.contains(&next_index) {
+						ent_indices.push(next_index);
+					}
+				}
+			}
+			
+			ent_indices.sort();
+			
+			let mut consumed_sized = 0;
+			let mut ent_iter = entities.join();
+			for ent_index in ent_indices {
+				let next_index = ent_index - consumed_sized;
+				consumed_sized = ent_index + 1;
+				if let Some(entity) = ent_iter.nth(next_index) {
+					entities.delete(entity).unwrap();
+				}
+			}
+			
 		}
 	}
 }
