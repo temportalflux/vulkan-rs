8e1d47fbc16fc67f27100c33acdbc4cb35aee674
Dustin Yost <dustin.yost.t@gmail.com>
2021-06-06 13:39:39 -0400

    Add vertex_object support for matrix spanning

--- derive/src/lib.rs
+++ derive/src/lib.rs
@@ -9,6 +9,19 @@ use syn::{
 	Fields, ItemStruct, Token,
 };
 
+#[derive(Debug)]
+struct SpanArgs {
+	size: usize,
+}
+
+impl Parse for SpanArgs {
+	fn parse(input: ParseStream) -> Result<Self> {
+		let literal = input.parse::<syn::LitInt>()?;
+		let size = literal.base10_parse::<usize>()?;
+		Ok(Self { size })
+	}
+}
+
 /// Arguments that go into a `vertex_attribute` proc-macro.
 /// Format: `[..], Bit#, DataType`
 /// Example: `[R, G, B], Bit8, UnsignedNorm`
@@ -55,6 +68,8 @@ enum VertexObjectKind {
 	Unnamed(usize),
 }
 
+/// Applies the `vertex::Object` trait to some struct.
+/// 
 /// Example:
 /// ```
 /// // The macro assumes that the pipeline and flags mods have been brought into scope.
@@ -64,11 +79,11 @@ enum VertexObjectKind {
 /// #[derive(Debug, Default)]
 /// pub struct Vertex {
 /// 	#[vertex_attribute([R, G], Bit32, SFloat)]
-/// 	pos: Vector4<f32>,
+/// 	pos: Vec4,
 /// 	#[vertex_attribute([R, G], Bit32, SFloat)]
-/// 	tex_coord: Vector4<f32>,
+/// 	tex_coord: Vec4,
 /// 	#[vertex_attribute([R, G, B, A], Bit32, SFloat)]
-/// 	color: Vector4<f32>,
+/// 	color: Vec4,
 /// }
 /// ```
 ///
@@ -98,6 +113,46 @@ enum VertexObjectKind {
 /// 	}
 /// }
 /// ```
+/// 
+/// You can also specify matrices using the `vertex_span` attribute:
+/// ```
+/// #[vertex_object]
+/// pub struct Instance {
+/// 	#[vertex_attribute([R, G, B, A], Bit32, SFloat)]
+/// 	#[vertex_span(4)] // indicates that this type is actually 4 attributes (in this case, 4 vec4s)
+/// 	pub model: Mat4,
+/// }
+/// ```
+/// which becomes something like
+/// ```
+/// use pipeline::state::vertex;
+/// impl vertex::Object for Vertex {
+/// 	fn attributes() -> Vec<vertex::Attribute> {
+/// 		use flags::{ColorComponent::*, format::{Bits::*, DataType::*}};
+/// 		let mut vec = Vec::new();
+/// 		let model_offset = vertex::offset_of!(Vertex, model);
+/// 		let model_span = std::mem::size_of::<Mat4>() / 4;
+/// 		vec.push(vertex::Attribute {
+/// 			offset: model_offset + (model_span * 0),
+/// 			format: flags::format::format(&[R, G, B, A], Bit32, SFloat),
+/// 		});
+/// 		vec.push(vertex::Attribute {
+/// 			offset: model_offset + (model_span * 1),
+/// 			format: flags::format::format(&[R, G, B, A], Bit32, SFloat),
+/// 		});
+/// 		vec.push(vertex::Attribute {
+/// 			offset: model_offset + (model_span * 2),
+/// 			format: flags::format::format(&[R, G, B, A], Bit32, SFloat),
+/// 		});
+/// 		vec.push(vertex::Attribute {
+/// 			offset: model_offset + (model_span * 3),
+/// 			format: flags::format::format(&[R, G, B, A], Bit32, SFloat),
+/// 		});
+/// 		vec
+/// 	}
+/// }
+/// ```
+/// 
 #[proc_macro_attribute]
 pub fn vertex_object(args: TokenStream, input: TokenStream) -> TokenStream {
 	// ensure the `#[vertex_object]` macro has no arguments
@@ -118,6 +173,29 @@ pub fn vertex_object(args: TokenStream, input: TokenStream) -> TokenStream {
 	.iter_mut()
 	.enumerate()
 	{
+		let field_identity = field
+			.ident
+			.as_ref()
+			.map(|name| VertexObjectKind::Named(name.clone()))
+			.unwrap_or(VertexObjectKind::Unnamed(field_idx));
+		let mut field_span = 1;
+		
+		// Find any `vertex_span` fields (which indicate how many attributes the field takes up)
+		if let Some(idx) = field
+			.attrs
+			.iter()
+			.position(|attr| attr.path.is_ident("vertex_span"))
+		{
+			// need to remove `vertex_attribute` so it doesnt cause issues https://github.com/rust-lang/rust/issues/81039
+			let attribute = field.attrs.remove(idx);
+			match attribute.parse_args::<SpanArgs>() {
+				Ok(span) => {
+					field_span = span.size;
+				}
+				Err(err) => return err.to_compile_error().into(),
+			}
+		}
+
 		// Find any `vertex_attribute` fields
 		if let Some(idx) = field
 			.attrs
@@ -127,15 +205,11 @@ pub fn vertex_object(args: TokenStream, input: TokenStream) -> TokenStream {
 			// need to remove `vertex_attribute` so it doesnt cause issues https://github.com/rust-lang/rust/issues/81039
 			let attribute = field.attrs.remove(idx);
 			// Collect all the attribute args into one array for processing into attribute configurations.
-			if let Ok(args) = attribute.parse_args::<AttributeArgs>() {
-				vertex_attribs.push((
-					field
-						.ident
-						.as_ref()
-						.map(|name| VertexObjectKind::Named(name.clone()))
-						.unwrap_or(VertexObjectKind::Unnamed(field_idx)),
-					args,
-				));
+			match attribute.parse_args::<AttributeArgs>() {
+				Ok(args) => {
+					vertex_attribs.push((field_identity, field.ty.clone(), args, field_span));
+				}
+				Err(err) => return err.to_compile_error().into(),
 			}
 		}
 	}
@@ -144,27 +218,52 @@ pub fn vertex_object(args: TokenStream, input: TokenStream) -> TokenStream {
 	let attrib_count = vertex_attribs.len();
 	// Convert the attribute args and identifiers into the quoted
 	// `vertex::Attribute` objects.
-	let attribute_quotes = vertex_attribs.into_iter().map(|(kind, args)| {
+	let attribute_quotes = vertex_attribs.into_iter().map(|(kind, ty, args, span)| {
 		// Can convert identifies of named-struct and tuple-struct fields
 		// by quoting the `syn::Ident` and `syn::Index` properties.
+		let make_ident = |suffix: &str| {
+			match kind {
+				VertexObjectKind::Named(ref ident) => {
+					quote::format_ident!("{}_{}", ident, suffix)
+				}
+				VertexObjectKind::Unnamed(ref idx) => {
+					let idx = syn::Index::from(*idx);
+					quote::format_ident!("{}_{}", idx, suffix)
+				}
+			}
+		};
 		let kind_ident = match kind {
-			VertexObjectKind::Named(ident) => {
+			VertexObjectKind::Named(ref ident) => {
 				quote! { #ident }
 			}
-			VertexObjectKind::Unnamed(idx) => {
-				let idx = syn::Index::from(idx);
+			VertexObjectKind::Unnamed(ref idx) => {
+				let idx = syn::Index::from(*idx);
 				quote! { #idx }
 			}
 		};
+		let size_ident = make_ident("size");
+		let offset_ident = make_ident("offset");
 		// Construct the attribute definition by quoting the `syn::Ident` fields passed in the attribute.
 		let AttributeArgs { color_comps, bit, data_type } = args;
-		quote! {
-			pipeline::state::vertex::Attribute {
-				offset: pipeline::state::vertex::offset_of!(#name, #kind_ident),
-				format: flags::format::format(&[ #(ColorComponent::#color_comps),* ], Bits::#bit, DataType::#data_type),
-			}
+		let mut quotes = Vec::new();
+		// Creates a handful of variables which indicate the span for each partial-attribute.
+		quotes.push(quote! {
+			let #offset_ident = pipeline::state::vertex::offset_of!(#name, #kind_ident);
+			let #size_ident = std::mem::size_of::<#ty>();
+			let #size_ident = #size_ident / #span;
+		});
+		// Add attributes to the list based on how many spans are supported
+		for i in 0..span {
+			quotes.push(quote! {
+				vec.push(pipeline::state::vertex::Attribute {
+					offset: #offset_ident + (#size_ident * #i),
+					format: flags::format::format(&[ #(ColorComponent::#color_comps),* ], Bits::#bit, DataType::#data_type),
+				});
+			});
 		}
-	});
+
+		quotes
+	}).flatten();
 
 	// Construct the final metaprogramming,
 	// implementing the `vertex::Object` trait for the struct.
@@ -175,7 +274,7 @@ pub fn vertex_object(args: TokenStream, input: TokenStream) -> TokenStream {
 			fn attributes() -> Vec<pipeline::state::vertex::Attribute> {
 				use flags::{ColorComponent, format::{Bits, DataType}};
 				let mut vec = Vec::with_capacity(#attrib_count);
-				#(vec.push(#attribute_quotes);)*
+				#(#attribute_quotes)*
 				vec
 			}
 		}
